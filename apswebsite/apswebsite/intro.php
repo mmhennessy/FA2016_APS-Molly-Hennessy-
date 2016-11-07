e5ae40b1dd033715adca2e00e29fff668b9368e5
Robert Algeo <rjalgeo@gmail.com>
2016-11-07 13:41:17 -0600

    PHP Fixes
    Set up PHP Menu

---
+++ apswebsite/apswebsite/intro.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+		<title>Wilderness Travel</title>
+		<link type="text/css" rel="stylesheet" href="styles.css">
+</head>
+
+		<header>
+
+			<?php include 'menu.php'; ?>
+
+		</header>
+
+		<body>
+
+			<h2>Welcome to our site!</h2>
+
+				<p>Wilderness travel is a popular pastime. However, there are many hazrds and choices that can deterome the outcome of a trip an explorer may face. This website covers the topics of climate, travel tips as well as health and safety concerns. If you have any questions or comments please contact us via our contact page.</p>
+
+		<section class="slides">
+
+			<img src="images/grass.jpg">
+			<img src="images/paradise.jpg">
+			<img src="images/tents.jpg">
+			<img src="images/gear.jpg">
+
+		</section>
+
+<!--This is sample of the animation I would like to use. source:
+	<iframe src="http://fiddle.jshell.net/alexbergin/3wkcc/74/show/" width="500" height="300" style="display:block;background-color:transparent;overflow:hidden;" allowTransparency="false" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+
+-->
+
+		</body>
+
+</html>
