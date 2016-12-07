7bf33b66b7cab44a891ee25089a26cc64a399863
mmhennessy <mhennessy@mcad.edu>
2016-12-07 02:32:53 -0600

    Add files via upload
    Javascript initial commit

--- apswebsite/apswebsite/intro.php
+++ apswebsite/apswebsite/intro.php
@@ -4,25 +4,25 @@
 		<link type="text/css" rel="stylesheet" href="styles.css">
 </head>
 
-
+		<header>
 
 			<?php include 'menu.php'; ?>
 
-		
+		</header>	
 
-		<body>
+		<body>	
 
 			<h2>Welcome to our site!</h2>
 
 				<p>Wilderness travel is a popular pastime. However, there are many hazrds and choices that can deterome the outcome of a trip an explorer may face. This website covers the topics of climate, travel tips as well as health and safety concerns. If you have any questions or comments please contact us via our contact page.</p>
 
-		<section class="slides">
+		<section class="slides">	
 
-			<img src="images/grass.jpg">
-			<img src="images/paradise.jpg">
-			<img src="images/tents.jpg">
-			<img src="images/gear.jpg">
-
+            <img class="a" src="images/grass.jpg" alt="Photo of Savannah">
+            <img class="b" src="images/paradise.jpg" alt="Photo of Tropical Rain Forrest">
+            <img class="c" src="images/tents.jpg" alt="Photo of Tents">
+            <img class="d" src="images/gear.jpg" alt="Photo of Camping Gear">   
+            
 		</section>
 
 <!--This is sample of the animation I would like to use. source:
@@ -30,6 +30,44 @@
 
 -->
 
+<section class="campfire">
+
+ <div id="letter"></div>
+        <div id="description"></div>
+
+        <div id="animation">
+            <div class="fire">
+                <div class="top">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class="middle">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class="bottom">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <div class="logs">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+
+</section>
+
+         <link type="text/js" rel="stylesheet" href="jquery.js">
+         <link type="text/js" rel="stylesheet" href="aps.js">
+
+
 		</body>
 
-</html>
+</html>		
