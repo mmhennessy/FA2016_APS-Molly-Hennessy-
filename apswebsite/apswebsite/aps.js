cc715919f4c90c4e4254a688f77f41f8b11ac0a5
Robert Algeo <rjalgeo@gmail.com>
2016-12-07 12:35:15 -0600

    Image Zoom Fixes

--- apswebsite/apswebsite/aps.js
+++ apswebsite/apswebsite/aps.js
@@ -1,29 +1,51 @@
 $(document).ready(function(){
-	$("a").click(function resise(e){
-		$this.css ({
-			height: "400px", width: "600px"
-		});
-		});
 
-	$("b").click(function resise(e){
-		$this.css ({
-			height: "400px", width: "600px"
-		});
-		});
+	console.log("This is the file aps.js speaking.");
+
+	$("body").on("click","img",function() {
+
+			console.log("CLICK!");
+
+			//This function will zoom in on an image AND and the class .zoom to that image
+			//That class is used to track if a zoomed image is the image that was clicked
 
 
-	$("c").click(function resise(e){
-		$this.css ({
-			height: "400px", width: "600px"
-		});
-		});
+			if ($(this).hasClass("zoom")) {
+				$(this).css({height: "200px", width: "300px"}).removeClass("zoom");
+			} else {
+				$("img.zoom").css({height: "200px", width: "300px"}).removeClass("zoom");
+				$(this).css({height: "400px", width: "600px"}).addClass("zoom");
+			}
 
 
-	$("d").click(function resise(e){
-		$this.css ({
-			height: "400px", width: "600px"
-		});
-		});
-	
 
-	))
+	});
+
+	// $("a").click(function resise(e){
+	// 	$this.css ({
+	// 		height: "400px", width: "600px"
+	// 	});
+	// 	});
+	//
+	// $("b").click(function resise(e){
+	// 	$this.css ({
+	// 		height: "400px", width: "600px"
+	// 	});
+	// 	});
+	//
+	//
+	// $("c").click(function resise(e){
+	// 	$this.css ({
+	// 		height: "400px", width: "600px"
+	// 	});
+	// 	});
+	//
+	//
+	// $("d").click(function resise(e){
+	// 	$this.css ({
+	// 		height: "400px", width: "600px"
+	// 	});
+	// 	});
+
+
+});
--- apswebsite/apswebsite/intro.php
+++ apswebsite/apswebsite/intro.php
@@ -8,21 +8,21 @@
 
 			<?php include 'menu.php'; ?>
 
-		</header>	
+		</header>
 
-		<body>	
+		<body>
 
 			<h2>Welcome to our site!</h2>
 
 				<p>Wilderness travel is a popular pastime. However, there are many hazrds and choices that can deterome the outcome of a trip an explorer may face. This website covers the topics of climate, travel tips as well as health and safety concerns. If you have any questions or comments please contact us via our contact page.</p>
 
-		<section class="slides">	
+		<section class="slides">
 
             <img class="a" src="images/grass.jpg" alt="Photo of Savannah">
             <img class="b" src="images/paradise.jpg" alt="Photo of Tropical Rain Forrest">
             <img class="c" src="images/tents.jpg" alt="Photo of Tents">
-            <img class="d" src="images/gear.jpg" alt="Photo of Camping Gear">   
-            
+            <img class="d" src="images/gear.jpg" alt="Photo of Camping Gear">
+
 		</section>
 
 <!--This is sample of the animation I would like to use. source:
@@ -64,10 +64,10 @@
 
 </section>
 
-         <link type="text/js" rel="stylesheet" href="jquery.js">
-         <link type="text/js" rel="stylesheet" href="aps.js">
+                <script src="jquery.js"></script>
+                <script src="aps.js"></script>
 
 
-		</body>
+        </body>
 
-</html>		
+</html>
