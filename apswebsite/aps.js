ecf65e23d55c1087a35bdb7134fd7d2556d4cfd1
mmhennessy <mhennessy@mcad.edu>
2016-12-14 11:24:12 -0600

    Add files via upload
    javascript file update

---
+++ apswebsite/aps.js
@@ -0,0 +1,9 @@
+$("body").on("click", "img", function() {
+	console.log("CLICK!");
+
+	if ($(this).hasClass("zoom")) {
+		$(this).css({height: "50%", width: "50%"}).removeClass("zoom");
+	} else {
+		$("img.zoom").css({height: "50%", width: "50%"}).removeClass("zoom");
+		$(this).css({height: "50%", width: "50%"}).addClass("zoom");
+}});
